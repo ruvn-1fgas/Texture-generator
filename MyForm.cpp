f64587e6fa57197711f31df9a4308f2df8f0790b
Рустам Расимович <horrorvanchugov@gmail.com>
2023-01-04 16:44:43 +0400

    Initial commit

---
+++ MyForm.cpp
@@ -0,0 +1,13 @@
+﻿#include "MyForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThread]
+void main(array<String^>^ arg) {
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+
+    Course::MyForm form;
+    Application::Run(% form);
+}
